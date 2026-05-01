2be56a1b8523301fecc4b6b533d71abbb2005ccb
hanyashi <richiee.rey12@gmail.com>
2026-05-02 01:47:30 +0800

    initialize flakes & remove light

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -8,8 +8,7 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      btw = "echo i use nixos btw";
-      nrs = "sudo nixos-rebuild switch --flake .#nixos"; 
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos"; 
     };
   };
 
--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -47,5 +47,4 @@ in
     };
   };
 
-  programs.light.enable = true; 
 }
