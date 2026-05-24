1c8690d79ca5a96e67f1d1b7193a9701fa92ff50
hanyashi <richiee.rey12@gmail.com>
2026-05-24 21:38:47 +0800

    limine themeing

--- modules/system/secure-boot.nix
+++ modules/system/secure-boot.nix
@@ -21,11 +21,14 @@
     term_foreground: c4c4c4
     term_palette: 0a0000;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4
     term_palette_bright: 4f4e4e;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4
+    term_margin: 0
+    term_margin_x: 0
+    term_margin_y: 0
+    background_path:
   '';
 
   boot.loader.limine.extraEntries = ''
-    /-Windows
-    //Windows 11
+    /Windows 11
     comment: Windows Boot Manager
     protocol: efi_chainload
     image_path: guid(24e9f9e4-d0b7-4c03-a678-4d2c3f144ea6):/EFI/Microsoft/Boot/bootmgfw.efi
