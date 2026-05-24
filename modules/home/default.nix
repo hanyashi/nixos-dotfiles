5ec13fc2ec62116f9cc3c1b9e230509f7be41287
hanyashi <richiee.rey12@gmail.com>
2026-05-24 20:59:14 +0800

    another attempt to fix the home manager backup conflict

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -4,15 +4,11 @@
   home.username = "hanyashi";
   home.homeDirectory = "/home/hanyashi";
   home.stateVersion = "25.11";
-
-  home.file.".gtkrc-2.0".force = true;
-  xdg.configFile."gtk-3.0/settings.ini".force = true;
-  xdg.configFile."gtk-4.0/settings.ini".force = true;
   
   programs.bash = {
     enable = true;
     shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
+      nrs = "rm ~/.gtkrc-2.0.backup && sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
     };
   };
 
