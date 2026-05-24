80eb19874d71f09feb48bd0fada2e8db456c0a0e
hanyashi <richiee.rey12@gmail.com>
2026-05-24 21:22:48 +0800

    limine theming

--- modules/system/secure-boot.nix
+++ modules/system/secure-boot.nix
@@ -13,14 +13,20 @@
 
   boot.loader.limine.extraConfig = ''
     interface_branding: NixOS Bootloader
-    interface_branding_color: 1
+    
+    interface_branding_color: 6
+    interface_help_color: 6
+    
     term_background: 0a0000
     term_foreground: c4c4c4
     term_palette: 0a0000;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4
     term_palette_bright: 4f4e4e;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4
+  '';
 
-    /Windows Boot Manager
-    comment: Windows 11
+  boot.loader.limine.extraEntries = ''
+    /-Windows
+    //Windows 11
+    comment: Windows Boot Manager
     protocol: efi_chainload
     image_path: guid(24e9f9e4-d0b7-4c03-a678-4d2c3f144ea6):/EFI/Microsoft/Boot/bootmgfw.efi
   '';
