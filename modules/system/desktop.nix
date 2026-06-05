7faa3fe089cadf1301c846cbbd094ad1b39b55ff
hanyashi <richiee.rey12@gmail.com>
2026-06-06 00:16:12 +0800

    add obs virtual cam

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -35,8 +35,12 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
   ];
+
+  boot.kernelModules = [ "v4l2loopback" ];
+
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
+
   security.polkit.enable = true;
 }
