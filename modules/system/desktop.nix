bec1a071983c63eab698f4b461774c7128c804f2
hanyashi <richiee.rey12@gmail.com>
2026-06-21 18:01:54 +0800

    add scrcpy

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -19,12 +19,12 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     libGL glib xorg.libX11 xorg.libXext libxkbcommon fontconfig freetype
-    xorg.libxcb xorg.xcbutil xorg.xcbutilwm xorg.xcbutilimage scrcpy
+    xorg.libxcb xorg.xcbutil xorg.xcbutilwm xorg.xcbutilimage
     xorg.xcbutilkeysyms xorg.xcbutilrenderutil xorg.libSM xorg.libICE
   ];
 
   environment.systemPackages = with pkgs; [
-    firefox discord rofi discord-ptb obs-studio vscode
+    firefox discord rofi discord-ptb obs-studio vscode scrcpy
     vlc spotify imagemagick lmms unrar unzip droidcam
   ];
 
