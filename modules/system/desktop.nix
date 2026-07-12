28e22766cdf6e960b041eda69ee716870b5f0993
hanyashi <richiee.rey12@gmail.com>
2026-07-13 01:58:16 +0800

    update gtk theming syntax

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -18,7 +18,7 @@
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
-    libGL glib xorg.libX11 xorg.libXext libxkbcommon fontconfig freetype
+    libGL glib libx11 libxext libxkbcommon fontconfig freetype
     xorg.libxcb xorg.xcbutil xorg.xcbutilwm xorg.xcbutilimage
     xorg.xcbutilkeysyms xorg.xcbutilrenderutil xorg.libSM xorg.libICE
   ];
