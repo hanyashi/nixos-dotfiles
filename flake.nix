06a8bd26d583f0e5db9c0bd66a82cdac8155c230
hanyashi <richiee.rey12@gmail.com>
2026-07-13 01:50:36 +0800

    update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "modular nixos config setup + flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     millennium.url = "github:SteamClientHomebrew/Millennium/next?dir=packages/nix";
     
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
