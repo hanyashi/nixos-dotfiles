4ab6e29e31ec8ddf39641ee6f0c76bc7fc17fefb
hanyashi <richiee.rey12@gmail.com>
2026-07-13 13:31:06 +0800

    update syntax to 26.05

--- modules/system/secure-boot.nix
+++ modules/system/secure-boot.nix
@@ -5,6 +5,7 @@
   boot.loader.limine.enable = true;
   boot.loader.limine.secureBoot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+ 
   # boot.lanzaboote = {
   #   enable = true;
   #   pkiBundle = "/var/lib/sbctl";
@@ -20,7 +21,6 @@
       background = "0a0000";
       foreground = "c4c4c4";
       palette = "0a0000;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4";
-      paletteBright = "4f4e4e;480811;620a17;800e20;840e18;911022;bc1428;c4c4c4";
       margin = 0;
     };
   };
